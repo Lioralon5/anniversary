8d860d108874523fd6b27bb0acd72acd1e619a3f
Lior Alon <lior.alon@audiocodes.com>
2025-03-18 17:53:18 +0200

    removed unused import

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,4 +1,4 @@
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer } from 'react-leaflet';
 
 const Map = () => (
   <MapContainer center={[latitude, longitude]} zoom={5} style={{ height: '100vh', width: '100%' }}>
